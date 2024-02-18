fa2469c3a9f9d6ede4f87236fdc4f32c62cd8dfd
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2024-02-18 23:40:45 +0900

    라우팅 설정
    react-router-dom을 이용해 라우팅연동을 하였습니다.

--- jwitter-reloaded/src/App.tsx
+++ jwitter-reloaded/src/App.tsx
@@ -1,5 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
