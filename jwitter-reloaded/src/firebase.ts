9e793c7e5941bc07844d3f552d28acf2f83a36eb
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2024-02-24 19:37:16 +0900

    아직 못하고있는중임
    ts,yarn을 사용중임

---
+++ jwitter-reloaded/src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWfeMrffeAiPrplrbQljIq5wKDDYLfiJQ",
+  authDomain: "jwitter-d058f.firebaseapp.com",
+  projectId: "jwitter-d058f",
+  storageBucket: "jwitter-d058f.appspot.com",
+  messagingSenderId: "756268474923",
+  appId: "1:756268474923:web:ee3fddb099664f0474006e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
